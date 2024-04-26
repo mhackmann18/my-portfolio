f6e4abea7a9fef44a7c6029e3f3b27205f990f3d
Matt Hackmann <mhackmann13@icloud.com>
2024-04-26 17:28:51 -0500

    add resume to build

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,12 @@ import dts from "vite-plugin-dts";
 export default defineConfig({
   build: {
     rollupOptions: {
-      input: ["index.html", "projects.html"],
+      input: [
+        "index.html",
+        "projects.html",
+        "src/assets/Matthew_Hackmann_Resume.pdf",
+        "src/assets/images/email-icon.png",
+      ],
     },
   },
   plugins: [dts()],
